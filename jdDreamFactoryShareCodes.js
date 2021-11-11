1cf3a05b48a3767b6918ed05827e56c2ee648781
Rhysn <cedaren@gmail.com>
2021-11-11 11:26:17 +0800

    惊喜工厂

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -6,7 +6,7 @@
 //云服务器腾讯云函数等NOde.js用户在此处填写东东萌宠的好友码。
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
-let shareCodes = [
+let shareCodes = ['XU6GKz30yCKA4LYvpnm5zw==@NTXGxnRwTQkr7rbDn08j4w==@q0aZPe-QA6AChOBXOoOMBA==@60y72tM8PjtxKeL4EMpTOQ==@J9Xl0ITTjl0-8QxxTj1fwQ==@TqIiJFACVwWUmovQes7F6g==@bmHS94glNAEU9WMavLn_og==@QDotuqdYVNrSa3atJZ_V9Q==@yn0ZjWK6NwsvEjIQjHUKGg==@V5M2S-SK8-rfSK6SxIxiqw==@Mzyy76C0ZYpK3jgJrVL-gg=='
 ]
 
 // 从日志获取互助码
